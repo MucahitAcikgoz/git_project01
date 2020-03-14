58c6de487f1c83559f8557802668a6bcbe869c84
Mucahit Acikgoz <mucahitacikgoz91@gmail.com>
2020-03-14 21:10:31 +0200

    second commit

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -2,8 +2,18 @@ package gitPackage;
 
 public class GitDay2 {
 
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+		
+		
+		
+		
 
 	}
 
